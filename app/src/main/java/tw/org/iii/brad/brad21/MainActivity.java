c697611eedc2cac521abfb610be84d6600f3ef82
user <brad@brad.tw>
2020-03-29 12:03:23 +0800

    4

--- app/src/main/java/tw/org/iii/brad/brad21/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad/brad21/MainActivity.java
@@ -1,11 +1,22 @@
 package tw.org.iii.brad.brad21;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.webkit.JavascriptInterface;
@@ -17,15 +28,49 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private EditText max;
+    private LocationManager lmgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    123);
+        }else{
+            init();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        init();
+    }
+
+    private void init(){
+        lmgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (!lmgr.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivityForResult(intent, 123);
+        }
+
         max = findViewById(R.id.max);
         webView = findViewById(R.id.webView);
         initWebView();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (!lmgr.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Log.v("brad", "no gps");
+        }
     }
 
     private void initWebView(){
@@ -41,6 +86,45 @@ public class MainActivity extends AppCompatActivity {
 
         //webView.loadUrl("https://www.iii.org.tw");
         webView.loadUrl("file:///android_asset/brad.html");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myListener = new MyListener();
+        lmgr.requestLocationUpdates(LocationManager.GPS_PROVIDER
+        ,0,0, myListener);
+    }
+    private MyListener myListener;
+    private class MyListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+            Log.v("brad", lat + ", " + lng);
+            webView.loadUrl(String.format("javascript:moveTo(%f,%f)", lat, lng));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        lmgr.removeUpdates(myListener);
     }
 
     @Override
